a91d5bbce1c44ea286b7da55627ebe47308b0b33
= <=>
2016-04-30 19:25:03 +0800

    update py code

--- README.md
+++ README.md
@@ -12,3 +12,12 @@ test py code
 ![image](http://www.liaoxuefeng.com/files/attachments/00138676512923004999ceca5614eb2afc5c0efdd2e4640000/0)
 
 ---
+
+> this is a block
+
+_this is a text_
+
+__this is a text__
+
+
+
--- pytest.py
+++ pytest.py
@@ -2,9 +2,13 @@
 
 import numpy as np
 import scipy as cp
-import scikit_learn as sk
+import pandas as pd
+import sklearn as sk
 import tensorflow as tf
 
 print('Hello World!')
 
 print('start Python and MachineLearning!')
+
+# edit in home
+
